0b1f921789ceae5535bf3713f856d838453dfe26
Reza Agustiana <reza.agustiana@gmail.com>
2020-02-11 10:51:26 +0700

    initialize notification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PagesContainerComponent } from './pages-container/pages-container.component';
 import { HomeComponent } from './home/home.component';
+import { ReminderComponent } from './reminder/reminder.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent },  
+  { path: 'notif/settings', component: ReminderComponent },
   { path: 'notif/:page/:position', component: PagesContainerComponent },
   { path: ':page', component: PagesContainerComponent },
 ];
